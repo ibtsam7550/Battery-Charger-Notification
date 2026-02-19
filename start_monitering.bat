1add7d610d7917517357a30a35fa50a28a12cb5c
M.Ibtsam <ibtsam7550@gmail.com>
2026-02-19 10:16:09 +0500

    Update start_monitering.bat

--- start_monitering.bat
+++ start_monitering.bat
@@ -1,6 +1,21 @@
+
 @echo off
 REM Change directory to script folder
 cd /d "C:\Users\19814\Desktop\New folder\Battery-Charger-Notification-main" #Enter the complete path of your folder where the Python script lies
 
-REM Run Python script silently
+REM --- Check & install required Python packages ---
+python - <<END
+import subprocess
+import sys
+
+packages = ["psutil", "plyer"]
+
+for package in packages:
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+END
+
+REM --- Run Python script silently ---
 start "" pythonw.exe battery_monitor.pyw
